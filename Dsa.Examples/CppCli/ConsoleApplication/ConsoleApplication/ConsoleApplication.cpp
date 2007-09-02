f07fe161cc7f238553ec3ab885789771e464b267
gbarnett_cp <SND\gbarnett_cp>
2007-09-02 21:04:05 +0000

    Added example of how to use SinglyLinkedListCollection(Of T) to the C++/CLI example project.

--- Dsa.Examples/CppCli/ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ Dsa.Examples/CppCli/ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -3,8 +3,42 @@
 #include "stdafx.h"
 
 using namespace System;
+using namespace Dsa::DataStructures;
 
-int main(array<System::String ^> ^args)
-{
+/*Creates a SinglyLinkedListCollection(Of T) and then performs some common operations on the
+collection.*/
+void SinglyLinkedListCollectionExample() {
+	SinglyLinkedListCollection<String ^>^ sll = gcnew SinglyLinkedListCollection<String ^>();
+	sll->AddLast("Bing");
+	sll->AddBefore(sll->Head, "Chandler");
+	sll->AddFirst("Monica");
+	sll->AddAfter(sll->Head, "Geller");
+	for each (String^ s in sll) {
+		Console::WriteLine(s);
+	}
+	array<String^>^ myArray = gcnew array<String^>(sll->Count);
+	sll->CopyTo(myArray);
+	Console::WriteLine();
+	for each (String^ s in myArray) {
+		Console::WriteLine(s);
+	}
+	Console::WriteLine();
+	Console::WriteLine("Contains Joey? {0}", sll->Contains("Joey"));
+	Console::WriteLine("Contains Monica? {0}", sll->Contains("Monica"));
+	sll->Remove("Chandler");
+	Console::WriteLine();
+	for each (String^ s in sll) {
+		Console::WriteLine(s);
+	}
+	sll->RemoveFirst();
+	sll->RemoveLast();
+	Console::WriteLine();
+	Console::WriteLine("Head value: {0}, Tail value: {1}", sll->Head->Value, sll->Tail->Value);
+}
+
+int main(array<System::String ^>^ args) {
+	SinglyLinkedListCollectionExample();
     return 0;
 }
+
+
