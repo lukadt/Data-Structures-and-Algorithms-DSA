d176f0fc3acaa0b7aa2e35d8259b58a0614c3626
gbarnett_cp <SND\gbarnett_cp>
2007-09-02 20:43:32 +0000

    Added example of how to use SinglyLinkedListCollection(Of T) to C# example app.

--- Dsa.Examples/CS/ConsoleApplication/ConsoleApplication/Program.cs
+++ Dsa.Examples/CS/ConsoleApplication/ConsoleApplication/Program.cs
@@ -1,10 +1,45 @@
 ﻿using System;
+using Dsa.DataStructures;
 
 namespace ConsoleApplication {
 
     class Program {
 
         static void Main(string[] args) {
+            SinglyLinkedListCollectionExample();
+        }
+
+        /// <summary>
+        /// Creates a SinglyLinkedListCollection(Of T) and then performs some common operations on the
+        /// collection.
+        /// </summary>
+        static void SinglyLinkedListCollectionExample() {
+            SinglyLinkedListCollection<string> sll = new SinglyLinkedListCollection<string>();
+            sll.AddLast("Bing");
+            sll.AddBefore(sll.Head, "Chandler");
+            sll.AddFirst("Monica");
+            sll.AddAfter(sll.Head, "Geller");
+            foreach (string s in sll) {
+                Console.WriteLine(s);
+            }
+            string[] myArray = new string[sll.Count];
+            sll.CopyTo(myArray);
+            Console.WriteLine();
+            foreach (string s in myArray) {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Contains Joey? {0}", sll.Contains("Joey"));
+            Console.WriteLine("Contains Monica? {0}", sll.Contains("Monica"));
+            sll.Remove("Chandler");
+            Console.WriteLine();
+            foreach (string s in sll) {
+                Console.WriteLine(s);
+            }
+            sll.RemoveFirst();
+            sll.RemoveLast();
+            Console.WriteLine();
+            Console.WriteLine("Head value: {0}, Tail value: {1}", sll.Head.Value, sll.Tail.Value);
         }
 
     }
