a77a173008e2811a9d55f87247f13f127f45fcb6
gbarnett_cp <SND\gbarnett_cp>
2007-09-05 18:32:45 +0000

    Added Push, Pop and Peek to StackCollection(Of T).

---
+++ Dsa/Dsa.Test/StackCollectionTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dsa.DataStructures;
+
+namespace Dsa.Test {
+
+    [TestClass]
+    public class StackCollectionTest {
+
+        /// <summary>
+        /// Test to see that Pushing an item onto the stack results in the intended behaviour.
+        /// </summary>
+        [TestMethod]
+        public void PushTest() {
+            StackCollection<int> myStack = new StackCollection<int>();
+
+            myStack.Push(10);
+            myStack.Push(20);
+
+            Assert.AreEqual<int>(20, myStack.Peek());
+        }
+
+        /// <summary>
+        /// Test to see that calling Peek returns the correct item.
+        /// </summary>
+        [TestMethod]
+        public void PeekTest() {
+            StackCollection<int> myStack = new StackCollection<int>();
+
+            myStack.Push(10);
+            myStack.Push(20);
+            myStack.Push(30);
+
+            Assert.AreEqual<int>(30, myStack.Peek());
+        }
+
+        /// <summary>
+        /// Test to see that the correct item is returned and the item at the top of the stack is 
+        /// updated appropriatley after calling Pop.
+        /// </summary>
+        [TestMethod]
+        public void PopTest() {
+            StackCollection<int> actual = new StackCollection<int>();
+
+            actual.Push(10);
+            actual.Push(20);
+            actual.Push(30);
+
+            Assert.AreEqual<int>(30, actual.Pop());
+            Assert.AreEqual<int>(20, actual.Peek());
+        }
+
+    }
+
+}
---
+++ Dsa/Dsa/StackCollection.cs
@@ -0,0 +1,39 @@
+﻿namespace Dsa.DataStructures {
+
+    public class StackCollection<T> {
+
+        private SinglyLinkedListCollection<T> _stack;
+
+        public StackCollection() {
+            _stack = new SinglyLinkedListCollection<T>();
+        }
+
+        /// <summary>
+        /// Pushes an item onto the top of the StackCollection.
+        /// </summary>
+        /// <param name="item">Item to push onto top of the StackCollection.</param>
+        public void Push(T item) {
+            _stack.AddLast(item);
+        }
+
+        /// <summary>
+        /// Returns the item at the top of the StackCollection.
+        /// </summary>
+        /// <returns>Item at the top of the StackCollection.</returns>
+        public T Peek() {
+            return _stack.Tail.Value;
+        }
+
+        /// <summary>
+        /// Removes and returns the item at the top of the StackCollection.
+        /// </summary>
+        /// <returns>Item at the top of the StackColleciton.</returns>
+        public T Pop() {
+            T peek = _stack.Tail.Value; 
+            _stack.RemoveLast();
+            return peek;
+        }
+
+    }
+
+}
