de0162587676f12581c772574dd972ecc57d5416
gbarnett_cp <SND\gbarnett_cp>
2007-09-12 17:54:02 +0000

    Added ArrayListCollection(Of T) including:  IndexOf, and Add methods as well as Capacity and Count properties.  Tests also included.

---
+++ Dsa/Dsa.Test/ArrayListCollectionTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dsa.DataStructures;
+
+namespace Dsa.Test {
+
+    [TestClass]
+    public class ArrayListCollectionTest {
+
+        /// <summary>
+        /// Test to see that Add returns the correct value.
+        /// </summary>
+        [TestMethod]
+        public void AddTest() {
+            ArrayListCollection<int> actual = new ArrayListCollection<int>();
+
+            Assert.AreEqual<int>(0, actual.Add(10));
+            Assert.AreEqual<int>(1, actual.Add(20));
+            Assert.AreEqual<int>(2, actual.Add(30));
+            Assert.AreEqual<int>(3, actual.Add(40));
+            Assert.AreEqual<int>(4, actual.Add(50));
+        }
+
+        /// <summary>
+        /// Test to see that Count returns the expected value.
+        /// </summary>
+        [TestMethod]
+        public void CountTest() {
+            ArrayListCollection<int> actual = new ArrayListCollection<int>();
+
+            actual.Add(10);
+            actual.Add(20);
+            actual.Add(30);
+            actual.Add(40);
+            actual.Add(50);
+
+            Assert.AreEqual<int>(5, actual.Count);
+        }
+
+        /// <summary>
+        /// Test to see that Capacity returns the expected value when no resixing occurs.
+        /// </summary>
+        [TestMethod]
+        public void CapacityNoResizeTest() {
+            ArrayListCollection<int> actual = new ArrayListCollection<int>();
+
+            Assert.AreEqual<int>(4, actual.Capacity);
+        }
+
+        /// <summary>
+        /// Test to see that Capacity returns the expected value when resizing has occurred.
+        /// </summary>
+        [TestMethod]
+        public void CapacityResizeTest() {
+            ArrayListCollection<int> actual = new ArrayListCollection<int>();
+
+            actual.Add(10);
+            actual.Add(20);
+            actual.Add(30);
+            actual.Add(40);
+            actual.Add(50);
+
+            Assert.AreEqual<int>(8, actual.Capacity);
+        }
+
+        /// <summary>
+        /// Test to see that the correct index is returned for an item in the ArrayListColleciton.
+        /// </summary>
+        [TestMethod]
+        public void IndexOfTest() {
+            ArrayListCollection<string> actual = new ArrayListCollection<string>();
+
+            actual.Add("London");
+            actual.Add("Paris");
+            actual.Add("New York");
+
+            Assert.AreEqual<int>(1, actual.IndexOf("Paris"));
+            Assert.AreEqual<int>(-1, actual.IndexOf("Prague"));
+        }
+
+    }
+
+}
---
+++ Dsa/Dsa/DataStructures/ArrayListCollection.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Dsa.DataStructures {
+    
+    public class ArrayListCollection<T> : ICollection<T>, IList, IList<T> {
+
+        private int _count;
+        private int _capacity = 4;
+        private T[] _items;
+
+        /// <summary>
+        /// Initializes a new instance of the ArrayListCollection class that is empty and has the default initial capacity.
+        /// </summary>
+        public ArrayListCollection() {
+            _items = new T[_capacity];
+        }
+
+        /// <summary>
+        /// Adds a value to the end of the ArrayListCollection(Of T).
+        /// </summary>
+        /// <param name="value">Value to add to the ArrayListCollection(Of T).</param>
+        /// <returns>The index of the ArrayListCollection(Of T) the value was added to.</returns>
+        public int Add(T value) {
+            if (_count == _capacity) {
+                Array.Resize<T>(ref _items, _capacity *= 2);
+            }
+            _items[_count] = value;
+            return _count++;
+        }
+
+        /// <summary>
+        /// Gets the capacity of the ArrayListCollection(Of T).
+        /// </summary>
+        public int Capacity {
+            get { return _capacity; }
+        }
+
+        #region IList Members
+
+        /// <summary>
+        /// Adds a value to the end of the ArrayListCollection(Of T).
+        /// </summary>
+        /// <param name="value">Value to add to the ArrayListCollection(Of T).</param>
+        /// <returns>The index of the ArrayListCollection(Of T) the value was added to.</returns>
+        int IList.Add(object value) {
+            throw new System.NotImplementedException();
+        }
+
+        void IList.Clear() {
+            throw new System.NotImplementedException();
+        }
+
+        bool IList.Contains(object value) {
+            throw new System.NotImplementedException();
+        }
+
+        int IList.IndexOf(object value) {
+            throw new System.NotImplementedException();
+        }
+
+        void IList.Insert(int index, object value) {
+            throw new System.NotImplementedException();
+        }
+
+        bool IList.IsFixedSize {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        bool IList.IsReadOnly {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        void IList.Remove(object value) {
+            throw new System.NotImplementedException();
+        }
+
+        void IList.RemoveAt(int index) {
+            throw new System.NotImplementedException();
+        }
+
+        object IList.this[int index] {
+            get {
+                throw new System.NotImplementedException();
+            }
+            set {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        #endregion
+
+        #region ICollection<T> Members
+
+        void ICollection<T>.Add(T item) {
+            throw new System.NotImplementedException();
+        }
+
+        void ICollection<T>.Clear() {
+            throw new System.NotImplementedException();
+        }
+
+        bool ICollection<T>.Contains(T item) {
+            throw new System.NotImplementedException();
+        }
+
+        void ICollection<T>.CopyTo(T[] array, int arrayIndex) {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the number of elements contained in the ICollection(Of T).
+        /// </summary>
+        public int Count {
+            get { return _count; }
+        }
+
+        bool ICollection<T>.IsReadOnly {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        bool ICollection<T>.Remove(T item) {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        #region IEnumerable<T> Members
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        #region ICollection Members
+
+        void ICollection.CopyTo(System.Array array, int index) {
+            throw new System.NotImplementedException();
+        }
+
+        bool ICollection.IsSynchronized {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        object ICollection.SyncRoot {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        #endregion
+
+        #region IList<T> Members
+
+        /// <summary>
+        /// Searches for the specified object and returns the zero-based index of the first occurrence within the entire ArrayListCollection(Of T).
+        /// </summary>
+        /// <param name="item">The object to locate in the ArrayListCollection(Of T). The value can be null for reference types.</param>
+        /// <returns>The zero-based index of the first occurrence of item within the entire ArrayListCollection(Of T), if found; otherwise, –1.</returns>
+        public int IndexOf(T item) {
+            return Array.IndexOf(_items, item);
+        }
+
+        void IList<T>.Insert(int index, T item) {
+            throw new NotImplementedException();
+        }
+
+        void IList<T>.RemoveAt(int index) {
+            throw new NotImplementedException();
+        }
+
+        T IList<T>.this[int index] {
+            get {
+                throw new NotImplementedException();
+            }
+            set {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+
+        #region ICollection<T> Members
+
+
+        int ICollection<T>.Count {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+    }
+
+}
