26a7fa7351cbc6451394827ba8759e61a2996b6e
gbarnett_cp <SND\gbarnett_cp>
2007-09-30 18:06:07 +0000

    Added Sorting file and associated tests.

---
+++ Dsa/Dsa.Test/SortingTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Dsa.Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dsa.Test {
+
+    [TestClass]
+    public class SortingTest {
+
+        /// <summary>
+        /// Test to see that the bubblesort algorithm sorts the items
+        /// in ascending order.
+        /// </summary>
+        [TestMethod]
+        public void BubbleSortAscTest() {
+            int[] myInts = { 23, 1, 44, 62, 1, 6, 90, 34 };
+            int[] actual = new int[myInts.Length];
+            int[] expected = { 1, 1, 6, 23, 34, 44, 62, 90 };
+
+            actual = myInts.BubbleSort();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Test to see that passing in a null array to BubbleSort results in the expected
+        /// exception being thrown.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BubbleSortNullArrayTest() {
+            int[] actual = null;
+
+            actual.BubbleSort();
+        }
+
+    }
+
+}
---
+++ Dsa/Dsa/Algorithms/Sorting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dsa.Algorithms {
+
+    /// <summary>
+    /// Sorting algorithms.
+    /// </summary>
+    public static class Sorting {
+
+        /// <summary>
+        /// Sorts an array of integers in ascending order.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
+        /// <returns>The sorted array.</returns>
+        public static int[] BubbleSort(this int[] array) {
+            if (array == null) {
+                throw new ArgumentNullException("array");
+            }
+            for (int i = 0; i < array.Length; i++) {
+                for (int j = 0; j < array.Length; j++) {
+                    int temp = array[j];
+                    if (array[i] < array[j]) {
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+    }
+
+}
