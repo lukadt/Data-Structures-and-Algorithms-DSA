174d4c0ca9468bc1989d3f5c2c2550b1f36e9cfb
gbarnett_cp <SND\gbarnett_cp>
2007-09-30 11:21:27 +0000

    Added Fibonacci and Factorial algorithms.

---
+++ Dsa/Dsa.Test/NumbersTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dsa.Algorithms;
+
+namespace Dsa.Test {
+
+    [TestClass]
+    public class NumbersTest {
+
+        /// <summary>
+        /// Test to see that calling Fibonacci algiorithm returns the correct value.
+        /// </summary>
+        [TestMethod]
+        public void FibonacciTest() {
+            Assert.AreEqual<int>(13, Numbers.Fibonacci(6));
+        }
+
+        /// <summary>
+        /// Test to see that calling Factorial algorithm returns the correct value.
+        /// </summary>
+        [TestMethod]
+        public void FactorialTest() {
+            Assert.AreEqual<int>(720, Numbers.Factorial(6));
+        }
+
+    }
+
+}
---
+++ Dsa/Dsa/Algorithms/Numbers.cs
@@ -0,0 +1,29 @@
+﻿namespace Dsa.Algorithms {
+
+    /// <summary>
+    /// Number algorithms.
+    /// </summary>
+    public static class Numbers {
+
+        /// <summary>
+        /// Computes the fibonnaci number of an integer.
+        /// </summary>
+        /// <param name="n">Int to compute fibonnacci number for.</param>
+        /// <returns>The fibonnacci number.</returns>
+        public static int Fibonacci(int n) {
+            if (n < 2) return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        /// <summary>
+        /// Computes factorial of a givne integer.
+        /// </summary>
+        /// <param name="n">The integer to compute the factorial of.</param>
+        public static int Factorial(int n) {
+            if (n == 1) return 1;
+            return n * Factorial(n - 1);
+        }
+
+    }
+
+}
