4b8fc430f672f5684db602eecbe9eb1e2b0f18cb
gbarnett_cp <SND\gbarnett_cp>
2007-11-05 21:28:19 +0000

    - Test project structure updated.  Now easier to navigate.

---
+++ Dsa/Dsa.Test/Util/CoordinateComparer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Dsa.Test
+{
+
+    /// <summary>
+    /// A simple stuct that exists so we can simply model a comparer for.
+    /// </summary>
+    public struct Coordinate
+    {
+        
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinate(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+
+    public class CoordinateComparer : IComparer<Coordinate>
+    {
+        /// <summary>
+        /// A dummy comparer.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Compare(Coordinate x, Coordinate y)
+        {
+            if (x.X == y.X && x.Y == y.Y)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
