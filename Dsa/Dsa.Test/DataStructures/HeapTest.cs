ffc2244b6d424bc998b904a1264ab4c2b5d97233
gbarnett_cp <SND\gbarnett_cp>
2008-01-22 13:42:06 +0000

    - Added Heap - Added Add (uses max heapify by default at the moment) - Added MaxHeapify method - Added .txt files for algorithms, and data structure pseudo files.  Deleted .pseudo counterparts

---
+++ Dsa/Dsa.Test/DataStructures/HeapTest.cs
@@ -0,0 +1,53 @@
+﻿using Dsa.DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dsa.Test.DataStructures
+{
+
+    /// <summary>
+    /// Tests for Heap.
+    /// </summary>
+    [TestClass]
+    public class HeapTest
+    {
+
+        /// <summary>
+        /// Check to see that adding an item to the Heap results in the correct behaviour.
+        /// </summary>
+        [TestMethod]
+        public void AddTest()
+        {
+            Heap<int> actual = new Heap<int>()
+            {
+                10,
+                20,
+                66,
+                21,
+                73
+            };
+
+            Assert.AreEqual(5, actual.Count);
+        }
+
+        /// <summary>
+        /// Check to see that MaxHeap affects the state of the Heap appropriatley.
+        /// </summary>
+        [TestMethod]
+        public void GetEnumeratorTest()
+        {
+            Heap<int> actual = new Heap<int>()
+            {
+                10,
+                23,
+                7,
+                9,
+                12,
+                18
+            };
+            int[] expected = { 23, 12, 18, 9, 10, 7 };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ Dsa/Dsa/DataStructures/Heap.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using Dsa.Utility;
+
+namespace Dsa.DataStructures
+{
+
+    /// <summary>
+    /// Heap data structure.
+    /// </summary>
+    /// <typeparam name="T">Type of heap.</typeparam>
+    public class Heap<T> : CollectionBase<T>
+    {
+
+        private T[] _heap;
+        private int _index;
+        private IComparer<T> _comparer;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="Heap{T}"/>.
+        /// </summary>
+        public Heap()
+        {
+            _heap = new T[4];
+            _comparer = Comparer<T>.Default;
+        }
+
+        /// <summary>
+        /// Adds an item to the <see cref="Heap{T}"/>.
+        /// </summary>
+        /// <param name="item">Item to add to the heap.</param>
+        public override void Add(T item)
+        {
+            if (_index == _heap.Length)
+            {
+                Array.Resize(ref _heap, 2 * _heap.Length);
+            }
+            _heap[_index++] = item;
+            Count++;
+            MaxHeapify();
+        }
+
+        /// <summary>
+        /// Clears the <see cref="Heap{T}"/> of its items.
+        /// </summary>
+        public override void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Determines whether or not the <see cref="Heap{T}"/> contains a specific item.
+        /// </summary>
+        /// <param name="item">Item to see if the Heap contains.</param>
+        /// <returns>True is the item is in the Heap; otherwise false.</returns>
+        public override bool Contains(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Removes an item from the <see cref="Heap{T}"/>.
+        /// </summary>
+        /// <param name="item">Item to remove from the Heap.</param>
+        /// <returns>True if the item was found and removed; otherwise false.</returns>
+        public override bool Remove(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Converts the <see cref="Heap{T}"/> to a one-dimension array.
+        /// </summary>
+        /// <returns>Array populated with the items from the Heap.</returns>
+        public override T[] ToArray()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the <see cref="Heap{T}"/> in breadth first order.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator{T}"/> for the <see cref="Heap{T}"/>.</returns>
+        public override IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _index; i++)
+            {
+                yield return _heap[i];
+            }
+        }
+
+        /// <summary>
+        /// Max heapifies the <see cref="Heap{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// The key of the parent is greater than or equal to that of its child, this property holds
+        /// throughout the Heap :. the key at the root of the Heap is the greatest key in the Heap.
+        /// </remarks>
+        private void MaxHeapify()
+        {
+            int i = _index - 1;
+            int parent = (i - 1) / 2;
+            while (i > 0 && Compare.IsGreaterThan(_heap[i], _heap[parent], _comparer))
+            {
+                T temp = _heap[i];
+                _heap[i] = _heap[parent];
+                _heap[parent] = temp;
+                i = parent;
+            }
+        }
+    }
+}
