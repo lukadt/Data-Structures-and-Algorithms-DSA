c9f8e55f9a8a22b8711fecd6031ae62a25c0e535
lukadt_cp <13589031+lukadt@users.noreply.github.com>
2008-07-31 23:58:43 +0000

    Added avl implementation (Avl.cs and AvlTreeNode.cs)

---
+++ Dsa/Dsa/DataStructures/AvlTreeNode.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="AvlTreeNode.cs" company="Data Structures and Algorithms">
+//   Copyright (C) Data Structures and Algorithms Team.
+// </copyright>
+// <summary>
+//   Generic implementation of an Avl node.
+// </summary>
+namespace Dsa.DataStructures
+{
+    /// <summary>
+    /// Node used by <see cref="Avl{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of the node.</typeparam>
+    public class AvlTreeNode<T> 
+    {
+        
+        /// <summary>
+        /// Get or sets the height of the node
+        /// </summary>
+        public int Height{ get; set; }
+        
+        /// <summary>
+        /// Gets or sets the left node reference.
+        /// </summary>
+        public AvlTreeNode<T> Left { get; set; }
+
+        /// <summary>
+        /// Gets or sets the right node reference.
+        /// </summary>
+        public AvlTreeNode<T> Right { get; set; }
+                
+        /// <summary>
+        /// Gets or sets the value of the node.
+        /// </summary>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// Creates and initializes a new instance of <see cref="AvlTreeNode{T}"/>.
+        /// </summary>
+        /// <param name="value">Value of node</param>
+        public AvlTreeNode(T value)               
+        {
+            Value = value;
+            Height = 0;
+        }
+    }
+}
