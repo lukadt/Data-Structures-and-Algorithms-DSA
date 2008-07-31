0e0c2739dd3c5d855513075b8d5059c33a1ececc
lukadt_cp <13589031+lukadt@users.noreply.github.com>
2008-07-31 23:59:52 +0000

    Added AvlTest.cs --- need further testing

--- Dsa/Dsa.Test/DataStructures/AvlTest.cs
+++ Dsa/Dsa.Test/DataStructures/AvlTest.cs
@@ -13,13 +13,23 @@ namespace Dsa.Test.DataStructures
         /// Check to see that the tree structure is correct after inserting nodes which will cause a rebalancing.
         /// </summary>
         [Test]
-        public void AddTest()
+        public void AddTestSingleRotationInvolved()
         {
             Avl<int> actual = new Avl<int> { 10, 20, 30 };
 
             Assert.AreEqual(20, actual.Root.Value);
             Assert.AreEqual(10, actual.Root.Left.Value);
             Assert.AreEqual(30, actual.Root.Right.Value);
+        }
+
+        [Test]
+        public void AddTestDoubleRotationInvolved()
+        {
+            Avl<int> actual = new Avl<int> { 10, 20, 15 };
+
+            Assert.AreEqual(15, actual.Root.Value);
+            Assert.AreEqual(10, actual.Root.Left.Value);
+            Assert.AreEqual(20, actual.Root.Right.Value);
         }
     }
 }
